829954b94edd3e5726ac62bc249d4a5e77604be8
kirill <kirill.dudchenko@zenzero.ae>
2022-09-19 15:22:16 +0700

    update mongo insert query;

--- app/db/mongodb/queries.go
+++ app/db/mongodb/queries.go
@@ -1,12 +1,15 @@
 package mongodb
 
-import "github.com/Ioloman/migration-script/app/models"
+import (
+	"github.com/Ioloman/migration-script/app/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func InsertLogs(logs *[]models.PaymentLog) error {
 	logsVal := *logs
 	insertLogs := make([]interface{}, len(logsVal))
-	for i := range logsVal {
-		insertLogs[i] = logsVal[i]
+	for i, log := range logsVal {
+		insertLogs[i] = bson.M{"payment_id": log.PaymentId, "date": log.Date, "text": log.Text}
 	}
 
 	_, err := Collection.InsertMany(CTX, insertLogs)
